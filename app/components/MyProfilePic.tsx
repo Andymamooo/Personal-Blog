5f00330ef42044f6c15773bd411ae8e4006fda35
Andymamooo <andyderkaema@gmail.com>
2023-06-06 07:13:12 -0700

    initial code for personal blog

---
+++ app/components/MyProfilePic.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+export default function MyProfilePic() {
+  return (
+    <section className = 'w-full mx-auto'>
+      <Image className = "border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+      src="/images/self.jpeg"
+      width={200}height={200}
+      alt="Andy Ma"
+      priority={true} />
+    </section>
+  )
+}
