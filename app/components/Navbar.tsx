113a0e0410932a588092d12064179683c78b66e3
Andymamooo <andyderkaema@gmail.com>
2023-06-06 20:48:05 -0700

    add blog feature , linkedin, github, and resume

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
-import {FaGithub, FaLinkedin} from "react-icons/fa"
+import {FaGithub, FaLinkedin, FaRegFilePdf } from "react-icons/fa"
+
 
 export default function Navbar() {
   return (
@@ -11,12 +12,16 @@ export default function Navbar() {
         </Link>
         </h1>
         <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
-                    <Link className="text-white/90 hover:text-white" href="https://github.com/Andymamooo">
-                        <FaGithub />
-                    </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://www.linkedin.com/in/a-ma/">
-                        <FaLinkedin />
-                    </Link>
+            <Link className="text-white/90 hover:text-white" href="https://github.com/Andymamooo">
+                <FaGithub />
+            </Link>
+            <Link className="text-white/90 hover:text-white" href="https://www.linkedin.com/in/a-ma/">
+                <FaLinkedin />
+            </Link>
+            <a className="text-white/90 hover:text-white" href= "/resume/Andy_Ma_Resume.pdf" target = "_blank">
+                <FaRegFilePdf />
+            </a>
+
         </div>
       </div>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Posts from "./components/Posts"
 
 export default function Home() {
   return (
     <main className = "px-6 mx-auto">
       <p className="mt-12 mb-12 text-5xl text-center dark:text-white">
         <span className = "whitespace-nowrap">
-           I'm <span className="font-bold">Andy 🐈</span>
+          Hi there,I&apos;m <span className="font-bold">Andy</span>.
         </span>
       </p>
+      <Posts/>
     </main>
   )
 }
